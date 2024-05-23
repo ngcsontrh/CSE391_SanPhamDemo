d13b69afc3803ed23d9d24fdabddf2ce5bbe49ad
thanvu1 <hoangthanvu11@gmail.com>
2024-05-23 16:32:38 +0700

    loi o Home

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Home from './pages/Home';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
