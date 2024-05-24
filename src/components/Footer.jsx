9df2365ea1750f4d6acd20c61f09d75aa4224c42
lyduybach2612 <lyduybach900@gmail.com>
2024-05-24 23:11:43 +0700

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import "../constants/Footer";
+import { GetToKnowUs,AmazonPayment } from "../constants/Footer";
+
+// function takeLink(prop) {
+//   return <ul>
+//     <a href={Object.keys(prop).map((key)=>prop[key])}><li>{Object.keys(prop).map((key)=>key)}</li></a>
+//   </ul>;
+// }
+
+function takeLink(prop) {
+  const keys = Object.keys(prop);
+  return (
+    <ul>
+      {keys.map((key) => (
+        <li key={key} className="text-white">
+          <a className="hover:underline" href={prop[key]}>{key}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function Footer() {
+  return (
+    <div className="flex flex-col">
+      <a href="#">
+        <div className="flex items-center justify-center text-xl pt-2 pb-2 text-white bg-slate-600 hover:bg-slate-500">
+          Back to top
+        </div>
+      </a>
+      <div className="bg-slate-800 flex  items-top justify-center">
+        <div className="p-12 mr-12">
+          <b className="text-white">GetToKnowUs</b>
+          {takeLink(GetToKnowUs)}
+        </div>
+
+        <div className="p-12 ml-12">
+          <b className="text-white">Amazon Payment</b>
+          {takeLink(AmazonPayment)}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Header from './components/Header'
 import './index.css'
-
+import Footer from './components/Footer'
+    
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
+    <Footer/>
   </React.StrictMode>,
 )
