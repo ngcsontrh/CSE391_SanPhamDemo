ca89f813cc2e5ca9295684adbd0d43a1375745bc
lyduybach2612 <lyduybach900@gmail.com>
2024-05-26 14:56:11 +0700

    final_entries_images

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,12 +1,7 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { Trend, TrendImage } from "../constants/Trending";
-import Pic1 from "../assets/images/trend1.jpg";
-import Pic2 from "../assets/images/trend2.jpg";
-import Pic3 from "../assets/images/trend3.jpg";
-import Pic4 from "../assets/images/trend4.jpg";
-import Pic5 from "../assets/images/trend5.jpg";
-import Pic6 from "../assets/images/trend6.jpg";
+import { TrendImage } from "../constants/Trending";
+
 export default function Trending() {
   const responsive = {
     desktop: {
@@ -36,24 +31,11 @@ export default function Trending() {
         dotListClass="custom-dot-list-style"
         itemClass="carousel-item-padding-40-px"
       >
-        <a href={TrendImage["Pic1"]}>
-          <img src={Pic1} alt="" />
-        </a>
-        <a href={TrendImage["Pic2"]}>
-          <img style={{ height: "15.5pc", width: "75%" }} src={Pic2} alt="" />
-        </a>
-        <a href={TrendImage["Pic3"]}>
-          <img src={Pic3} alt="" />
-        </a>
-        <a href={TrendImage["Pic4"]}>
-          <img src={Pic4} alt="" />
-        </a>
-        <a href={TrendImage["Pic5"]}>
-          <img src={Pic5} alt="" />
-        </a>
-        <a href={TrendImage["Pic6"]}>
-          <img src={Pic6} alt="" />
-        </a>
+        {Object.entries(TrendImage).map(([img,link]) => (
+            <a href={link} target="_blank">
+                <img src={img}  alt="" />
+            </a>
+        ))}
       </Carousel>
     </div>
   );
--- src/constants/Trending.js
+++ src/constants/Trending.js
@@ -1,3 +1,10 @@
+import Pic1 from "../assets/images/trend1.jpg";
+import Pic2 from "../assets/images/trend2.jpg";
+import Pic3 from "../assets/images/trend3.jpg";
+import Pic4 from "../assets/images/trend4.jpg";
+import Pic5 from "../assets/images/trend5.jpg";
+import Pic6 from "../assets/images/trend6.jpg";
+
 export const Trend = {
     "Amazon.com eGift Card": "Amazon",
     "Visa $100 Gift Card (plus $5.95 Purchase Fee)": "Visa",
@@ -8,15 +15,15 @@ export const Trend = {
 }
 
 export const TrendImage = {
-    Pic1 : "https://www.amazon.com/Amazon-eGift-Card-Smile-Spring/dp/B0CTR37B5G/ref=s9_acsd_al_ot_c2_x_0_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
+    [Pic1] : "https://www.amazon.com/Amazon-eGift-Card-Smile-Spring/dp/B0CTR37B5G/ref=s9_acsd_al_ot_c2_x_0_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
     
-    Pic2 : "https://www.amazon.com/Visa-Gift-Card-plus-Purchase/dp/B01MSBQB1P/ref=s9_acsd_al_ot_c2_x_1_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
+    [Pic2] : "https://www.amazon.com/Visa-Gift-Card-plus-Purchase/dp/B01MSBQB1P/ref=s9_acsd_al_ot_c2_x_1_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
     
-    Pic3 : "https://www.amazon.com/Amazon-eGift-Card-Thanks-Much/dp/B09RX1DBJP/ref=s9_acsd_al_ot_c2_x_2_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
+    [Pic3] : "https://www.amazon.com/Amazon-eGift-Card-Thanks-Much/dp/B09RX1DBJP/ref=s9_acsd_al_ot_c2_x_2_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
     
-    Pic4 : "https://www.amazon.com/Amazon-eGift-Card-Milestone-Birthday/dp/B09KXT1HLH/ref=s9_acsd_al_ot_c2_x_3_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
+    [Pic4] : "https://www.amazon.com/Amazon-eGift-Card-Milestone-Birthday/dp/B09KXT1HLH/ref=s9_acsd_al_ot_c2_x_3_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
     
-    Pic5 : "https://www.amazon.com/Amazon-1_US_Email-eGift-Card/dp/B004LLIKVU/ref=s9_acsd_al_ot_c2_x_4_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
+    [Pic5] : "https://www.amazon.com/Amazon-1_US_Email-eGift-Card/dp/B004LLIKVU/ref=s9_acsd_al_ot_c2_x_4_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
     
-    Pic6 : "https://www.amazon.com/Amazon-eGift-Card-Logo/dp/B07PCMWTSG/ref=s9_acsd_al_ot_c2_x_5_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
+    [Pic6] : "https://www.amazon.com/Amazon-eGift-Card-Logo/dp/B07PCMWTSG/ref=s9_acsd_al_ot_c2_x_5_t?_encoding=UTF8&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-9&pf_rd_r=N58CHBF8BC61V664AB4X&pf_rd_p=55150d55-e517-49f6-8767-fb99a0d4b127&pf_rd_t=&pf_rd_i=2238192011",
 }
