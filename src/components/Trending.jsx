b49640d0789e9d0a880df314e68da3dc3a5e8228
lyduybach2612 <lyduybach900@gmail.com>
2024-05-26 14:34:02 +0700

    final

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,12 +1,12 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Trend, TrendImage } from "../constants/Trending";
-import Pic1 from "../assets/images/trend1.jpg"
-import Pic2 from "../assets/images/trend2.jpg"
-import Pic3 from "../assets/images/trend3.jpg"
-import Pic4 from "../assets/images/trend4.jpg"
-import Pic5 from "../assets/images/trend5.jpg"
-import Pic6 from "../assets/images/trend6.jpg"
+import Pic1 from "../assets/images/trend1.jpg";
+import Pic2 from "../assets/images/trend2.jpg";
+import Pic3 from "../assets/images/trend3.jpg";
+import Pic4 from "../assets/images/trend4.jpg";
+import Pic5 from "../assets/images/trend5.jpg";
+import Pic6 from "../assets/images/trend6.jpg";
 export default function Trending() {
   const responsive = {
     desktop: {
@@ -18,9 +18,9 @@ export default function Trending() {
 
   return (
     <div style={{ padding: "20px" }}>
-        <div className="p-5 text-4xl font-semibold border-b m-6">
-            Trending now
-        </div>
+      <div className="p-5 text-4xl font-semibold border-b m-6">
+        Trending now
+      </div>
       <Carousel
         swipeable
         draggable
@@ -37,22 +37,22 @@ export default function Trending() {
         itemClass="carousel-item-padding-40-px"
       >
         <a href={TrendImage["Pic1"]}>
-            <img src={Pic1} alt="" />
+          <img src={Pic1} alt="" />
         </a>
         <a href={TrendImage["Pic2"]}>
-            <img style={{height: "15.5pc", width: "75%"}} src={Pic2} alt="" />
+          <img style={{ height: "15.5pc", width: "75%" }} src={Pic2} alt="" />
         </a>
         <a href={TrendImage["Pic3"]}>
-            <img src={Pic3} alt="" />
+          <img src={Pic3} alt="" />
         </a>
         <a href={TrendImage["Pic4"]}>
-            <img src={Pic4} alt="" />
+          <img src={Pic4} alt="" />
         </a>
         <a href={TrendImage["Pic5"]}>
-            <img src={Pic5} alt="" />
+          <img src={Pic5} alt="" />
         </a>
         <a href={TrendImage["Pic6"]}>
-            <img src={Pic6} alt="" />
+          <img src={Pic6} alt="" />
         </a>
       </Carousel>
     </div>
